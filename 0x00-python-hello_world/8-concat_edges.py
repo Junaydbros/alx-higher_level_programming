7a96e13a92bbcd809ee99db1bc68cbe926ee41c1
Junaydbros <hamidejisanusi@gmail.com>
2023-08-11 21:08:09 +0000

    Test

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming 
+str = "Python is an interpreted, interactive, object-oriented programming
 \tlanguage that combines remarkable power with very clear syntax"
-str = str[39:67] + 'with Python'
+str = str[39:66] + ' with Python'
 print(str)

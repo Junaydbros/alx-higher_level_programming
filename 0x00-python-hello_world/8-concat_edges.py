485529a971c4b03b2c5445c2188910925530640d
Junaydbros <hamidejisanusi@gmail.com>
2023-08-14 21:39:49 +0000

    Attempted fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming
+str = "Python is an interpreted, interactive, object-oriented programming m
 \tlanguage that combines remarkable power with very clear syntax"
-str = str[39:EOL] + ' with Python'
+str = str[39:67] + ' with Python'
 print(str)

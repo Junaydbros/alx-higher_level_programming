092f10003ff48bcde2ebfc67a0f59b0763ec1473
Junaydbros <hamidejisanusi@gmail.com>
2023-08-19 12:12:30 +0000

    Task 12-fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+
+def fizzbuzz():
+    for numb in range(100):
+        if numb % 3 == 0:
+            print("Fizz", end=" ")
+        elif numb % 5 == 0:
+            print("Buzz", end=" ")
+        elif numb % 3 == 0 and numb % 5 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print("{}".format(numb), end=" ")

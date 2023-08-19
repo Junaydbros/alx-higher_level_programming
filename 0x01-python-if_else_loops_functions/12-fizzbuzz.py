be49b6f0d18e792df00ed1559e3bf88cb491b202
Junaydbros <hamidejisanusi@gmail.com>
2023-08-19 12:34:54 +0000

    Fixed!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 
 def fizzbuzz():
     for numb in range(1, 101):
-        if numb % 3 == 0:
-            print("Fizz", end=" ")
+        if numb % 15 == 0:
+            print("FizzBuzz", end=" ")
         elif numb % 5 == 0:
             print("Buzz", end=" ")
-        elif numb % 15 == 0:
-            print("FizzBuzz", end=" ")
+        elif numb % 3 == 0:
+            print("Fizz", end=" ")
         else:
             print("{}".format(numb), end=" ")

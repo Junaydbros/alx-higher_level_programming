c8ac7a714291d48416991670990e146da7bf86ad
Junaydbros <hamidejisanusi@gmail.com>
2023-08-19 12:00:18 +0000

    LLL

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
+
 def uppercase(str):
     for ch in str:
-        if not ('A' <= str <= 'Z'):
-            try:
-                strInCaps = ord(ch) - 32
-            #print("{}".format(chr(strInCaps)), end="")
-            except ('A' <= str <= 'Z'):
-                #strInCaps = ord(ch)
-                pass
-            print("{}".format(chr(strInCaps)), end="")
-        else:
-            return str
-    #print("{}".format(chr(strInCaps)), end="")
+        if (ord(ch) >= 97 and ord(ch) <= 122):
+            strInCaps = ord(ch) - 32
+
+        print("{}".format(chr(strInCaps)), end="")
     print()
--- 0x01-python-if_else_loops_functions/testMain.py
+++ 0x01-python-if_else_loops_functions/testMain.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
+uppercase = __import__('8-uppercase').uppercase
 
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+uppercase("best")
+uppercase("Best School 98 Battery street")
+
+add = __import__('10-add').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
+
+pow = __import__('11-pow').pow
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))

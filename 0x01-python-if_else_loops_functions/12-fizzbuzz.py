38404b0a9bc50585f99833c70bf3dfe704e8eddc
Junaydbros <hamidejisanusi@gmail.com>
2023-08-19 12:22:37 +0000

    Fixed pending output error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for numb in range(100):
-        if numb % 3 == 0:
+    for numb in range(1, 101):
+        if numb % 3 == 0 and numb % 5:
+            print("FizzBuzz", end=" ")
+        elif numb % 3 == 0:
             print("Fizz", end=" ")
         elif numb % 5 == 0:
             print("Buzz", end=" ")
-        elif numb % 3 == 0 and numb % 5 == 0:
-            print("FizzBuzz", end=" ")
         else:
             print("{}".format(numb), end=" ")

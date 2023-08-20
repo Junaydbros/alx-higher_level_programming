1465ac5f7fd6817c577093d08e44837aa85a6fb0
Junaydbros <hamidejisanusi@gmail.com>
2023-08-20 00:56:50 +0000

    Task 2-args - corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
 
     print("{} arguments".format(len(argv)), end="")
 
-    for i, arg in enumerate(argv):
-        if len(argv) == 0:
-            print(".")
-        elif len(argv) > 0:
-            print(": ")
+    if len(argv) == 0:
+        print(".")
+    elif len(argv) > 0:
+        print(": ")
 
-        print("{}: ".format(i + 1), arg)
+    for i, arg in enumerate(argv):
+        print("{}: ".format((i + 1), arg))

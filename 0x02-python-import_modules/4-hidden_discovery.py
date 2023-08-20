af42ba864f9fc5ce7feb41088d06d3866a381648
Abdulhamid <hamidejisanusi@gmail.com>
2023-08-20 23:56:45 +0000

    Task 4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 
+import hidden_4 as hidden
+if __name__ == "__main__":
+    for name in dir(hidden):
+        if name.startswith("_"):
+            continue
+        print("{}".format(name))

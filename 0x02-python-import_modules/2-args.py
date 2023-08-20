138f4ffaa386624094bf4a7be99a3c90714d9b29
Junaydbros <hamidejisanusi@gmail.com>
2023-08-20 01:07:05 +0000

    New update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
+import sys
+#if __name__ == "__main__":
+
+    def argPrinter(*argv):
+        print("{} arguments".format(len(argv)), end="")
+
+        if len(argv) == 0:
+            print(".")
+        elif len(argv) > 0:
+            print(": ")
+
+        for i, arg in enumerate(argv):
+            print("{}: ".format((i + 1), arg))
+
 if __name__ == "__main__":
-
-    print("{} arguments".format(len(argv)), end="")
-
-    if len(argv) == 0:
-        print(".")
-    elif len(argv) > 0:
-        print(": ")
-
-    for i, arg in enumerate(argv):
-        print("{}: ".format((i + 1), arg))
+    arg = sys.args[1:]
+    argPrinter(*arg)

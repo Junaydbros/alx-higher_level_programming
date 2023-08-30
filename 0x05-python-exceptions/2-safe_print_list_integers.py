889b1c5e90f38f48e3c06796ed07915c190745d8
Abdulhamid <hamidejisanusi@gmail.com>
2023-08-30 21:02:46 +0000

    FIx

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             cnt = cnt + 1
         except (ValueError, TypeError):
             pass
-        print()
-        return cnt
+    print()
+    return cnt

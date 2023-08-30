59e974bb463abb167bae47f58ab9089fc762ee36
Abdulhamid <hamidejisanusi@gmail.com>
2023-08-30 20:56:05 +0000

    Err

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for index in range(x):
         try:
             print(f"{my_list[index]}", end="")
-            cnt++
+            cnt = cnt + 1
         except IndexError:
             break
     print()
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for index in range(x):
         try:
             print("{:d}".format(my_list[index]), end="")
-            cnt++
+            cnt = cnt + 1
         except (ValueError, TypeError):
             pass
         print()

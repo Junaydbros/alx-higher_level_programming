5db9e0a9fa30d5ce001cae62c4e59310ed7b8591
Abdulhamid <hamidejisanusi@gmail.com>
2023-08-30 00:13:29 +0000

    TAsk 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    for integer in my_list:
-        if idx < 0:
-            pass
-        if idx > len(my_list):
-            pass
-        element[idx] = integer
+    if idx < 0:
+        return my_list
+    if idx > len(my_list):
+        return my_list
+    my_list[idx] = element

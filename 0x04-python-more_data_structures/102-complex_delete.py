c61e16fa260a629ecd2a56b0d4059de5594bab32
Abdulhamid <hamidejisanusi@gmail.com>
2023-08-30 16:01:56 +0000

    Tasks 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def complex_delete(dictionary, value):
-    deletableKeys = [key for key, digit in a_dictionary.items() if digit == value]
+def complex_delete(a_dictionary, value):
+    deletableKeys = [key for key, param in a_dictionary.items() if param == value]
 
     for key in deletableKeys:
         del a_dictionary[key]
